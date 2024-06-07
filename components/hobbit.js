522f957ce9bca8ae5ce0233363a5f0d26ce9cdcf
gregularjoe <117665462+gregularjoe@users.noreply.github.com>
2024-06-06 21:45:32 -0230

    Added button component and hobbits component. Hobbit component and buttons render on screen. 3 buttons allow you to switch between each of the three very fine hobbitses.

---
+++ components/hobbit.js
@@ -0,0 +1,25 @@
+import { Image, Text } from 'react-native';
+
+export default function Hobbit( props ) {
+
+    return (
+        <>
+            <Text style={{fontSize: 30}}>{props.hobbits[props.hobbitIndex].name}</Text>
+            <Image source={props.image}
+                style={
+                    {
+                        width: 250,
+                        height: 250,
+                        borderRadius: 100
+                    }
+                }
+            />
+            <Text style={{fontSize: 17, marginHorizontal: 5}}>Actor: {props.hobbits[props.hobbitIndex].actor}</Text>
+            <Text style={{fontSize: 17, marginHorizontal: 5}}>Best Friend: {props.hobbits[props.hobbitIndex].bestFriend}</Text>
+            <Text style={{fontSize: 17, marginHorizontal: 20}}>Best Quote: {props.hobbits[props.hobbitIndex].bestLine}</Text>
+            {
+
+            }
+        </>
+    );
+}
